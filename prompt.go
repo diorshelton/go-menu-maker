68deea5a7234468aa6b7b1d6cf2f7dc6d5a71739
Dior.Shelton <dior.shelton@gmail.com>
2025-06-19 18:02:44 -0400

    add loop to prompt for retries, handle errors, and call CreateItem method

--- prompt.go
+++ prompt.go
@@ -13,20 +13,45 @@ func Prompt(q []string) string {
 }
 
 func ItemPrompt() Item {
+
+	var itemName string
+	var itemPrice float64
+
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("What is the item name?")
-	//Handle error
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	for {
+		fmt.Print("What is the item name? ")
+		//Handle error
+		name, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("an error occurred %v", err)
+		}
+		itemName = strings.TrimSpace(name)
+		// var cost float64
+		fmt.Print("What is the item cost? ")
+		cost, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("an error occurred %v", err)
+		}
+		cost = strings.TrimSpace(cost)
+		itemPrice, err = strconv.ParseFloat(cost, 64)
+		if err != nil {
+			fmt.Printf("an error occurred %v", err)
+		}
 
-	// var cost float64
-	fmt.Print("What is the item cost?")
-	cost, _ := reader.ReadString('\n')
-	cost = strings.TrimSpace(cost)
-	price, _ := strconv.ParseFloat(cost, 64)
-
-	item := Item{Name: name, Price: price}
-	fmt.Print(item)
+		// Verify item details
+		fmt.Printf("Is this information correct? (y/n) \n")
+		fmt.Printf("%v %v ? \n", itemName, itemPrice)
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("an error occurred %v", err)
+		}
+		response = strings.TrimSpace(response)
+		if response == "y" {
+			break
+		}
+	}
+	item := Item{Name: itemName, Price: itemPrice}
+	fmt.Printf("%v", item)
 	return item
 }
