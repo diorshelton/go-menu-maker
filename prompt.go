5e5a94ed87ebd57751a78e4f0c2fd8bad631aaa9
Dior.Shelton <dior.shelton@gmail.com>
2025-06-19 18:31:13 -0400

    make console messages more descriptive

--- prompt.go
+++ prompt.go
@@ -31,27 +31,29 @@ func ItemPrompt() Item {
 		fmt.Print("What is the item cost? ")
 		cost, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Printf("an error occurred %v", err)
+			fmt.Printf("An error occurred while reading the item name %v \n", err)
+			continue
 		}
 		cost = strings.TrimSpace(cost)
 		itemPrice, err = strconv.ParseFloat(cost, 64)
 		if err != nil {
-			fmt.Printf("an error occurred %v", err)
+			fmt.Printf("Invalid cost, '%s'.Please enter a valid number.\n", cost)
+			continue
 		}
 
 		// Verify item details
-		fmt.Printf("Is this information correct? (y/n) \n")
-		fmt.Printf("%v %v ? \n", itemName, itemPrice)
+		fmt.Printf("Is this information correct? (y/n)\n%v %.2f ?\n", itemName, itemPrice)
 		response, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Printf("an error occurred %v", err)
+			fmt.Printf("An error occurred while reading confirmation %v\n", err)
+			continue
 		}
-		response = strings.TrimSpace(response)
-		if response == "y" {
+		response = strings.TrimSpace(strings.ToLower(response))
+		if response == "y" || response == "yes" {
 			break
 		}
 	}
 	item := Item{Name: itemName, Price: itemPrice}
-	fmt.Printf("%v", item)
+	fmt.Printf("Item created: %+v\n", item)
 	return item
 }
